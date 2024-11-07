7d70c9c8e9f7d063023af8ef72913ec9b6b28685
alissoncesar <alissoncesar1113@gmail.com>
2024-11-07 01:29:03 -0300

    concluido

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.querySelector("button").onclick= () => {
     document.body.classList.toggle("dark-mode")
 }
+
+const input = document.getElementById('meuInput');
+const botao = document.getElementById('mostrarInput');
+
+botao.addEventListener('click', () => {
+  // Inverte o estado de exibição do input
+  input.style.display = input.style.display === 'none' ? 'block' : 'none';
+});
